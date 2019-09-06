36aefe2e4918dbd65f80668add2d0a361bc489ac
Igrom <igor@sowinski.blue>
2019-09-06 17:45:03 +0200

    Rework FullStory wrapper, add tests, move to linter `standard`

--- src/sentryWrapper.js
+++ src/sentryWrapper.js
@@ -1,5 +1,5 @@
 import * as Sentry from '@sentry/browser';
-import {isLocalhost} from './utils.js';
+import { isLocalhost } from './utils.js';
 
 let initialized = false;
 
@@ -12,25 +12,25 @@ let initialized = false;
  */
 
 const initSentry = (sentrySettings) => {
-    if (initialized) {
-        console.warn('Sentry already initialized.');
-        return;
-    }
-    initialized = true;
+  if (initialized) {
+    console.warn('Sentry already initialized.');
+    return;
+  }
+  initialized = true;
 
-    if (isLocalhost()) {
-        return;
-    }
-    Sentry.init({
-        dsn: sentrySettings.dsn,
-        release: sentrySettings.releaseVersion,
-        attachStacktrace: true,
-        debug: true,
-    });
+  if (isLocalhost()) {
+    return;
+  }
+  Sentry.init({
+    dsn: sentrySettings.dsn,
+    release: sentrySettings.releaseVersion,
+    attachStacktrace: true,
+    debug: true
+  });
 
-    Sentry.configureScope((scope) => {
-        scope.setLevel(sentrySettings.scope || 'warning');
-    });
+  Sentry.configureScope((scope) => {
+    scope.setLevel(sentrySettings.scope || 'warning');
+  });
 };
 
 /**
@@ -41,42 +41,42 @@ const initSentry = (sentrySettings) => {
    * }
 */
 const updateProfile = (profile) => {
-    if (!initialized) {
-        console.error('FullStory not initialized.');
-        return;
-    }
+  if (!initialized) {
+    console.error('FullStory not initialized.');
+    return;
+  }
 
-    if (isLocalhost() || !profile) {
-        return;
-    }
+  if (isLocalhost() || !profile) {
+    return;
+  }
 
-    Sentry.configureScope((scope) => {
-        scope.setUser({email: profile.email || profile.user_id});
-    });
+  Sentry.configureScope((scope) => {
+    scope.setUser({ email: profile.email || profile.user_id });
+  });
 };
 
 const reportError = (error, errorInfo) => {
-    if (!initialized) {
-        console.error('Sentry is not initialized. Not reporting the error.');
-        return;
-    }
+  if (!initialized) {
+    console.error('Sentry is not initialized. Not reporting the error.');
+    return;
+  }
 
-    if (!isLocalhost()) {
-        Sentry.withScope((scope) => {
-            Object
-                .keys(errorInfo || {})
-                .forEach((key) => {
-                    scope.setExtra(key, errorInfo[key]);
-                });
-            Sentry.captureException(error);
+  if (!isLocalhost()) {
+    Sentry.withScope((scope) => {
+      Object
+        .keys(errorInfo || {})
+        .forEach((key) => {
+          scope.setExtra(key, errorInfo[key]);
         });
-    } else {
-        console.error(error, errorInfo);
-    }
+      Sentry.captureException(error);
+    });
+  } else {
+    console.error(error, errorInfo);
+  }
 };
 
 export {
-    initSentry,
-    reportError,
-    updateProfile,
+  initSentry,
+  reportError,
+  updateProfile
 };
